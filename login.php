6962efb29af9555a4d9ab6f66d87f2920a89f904
denny <dennystw1@gmail.com>
2017-01-13 04:08:04 +0700

    bikin form login dan registrasinya

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <title>Wellcome to peroy library</title>
+      <style type="text/css">
+        @import "css/login.css";
+      </style>
+  </head>
+  <body bgcolor="#E3D8D8">
+      <div class="form-style-6">
+        <h1>LOGIN</h1>
+        <form>
+          <input type="text" name="username" placeholder="username" />
+          <input type="password" name="password" placeholder="password" />
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+
+      <center>
+        <i>_OR_</i>
+        <br><br>
+      </center>
+
+      <div class="form-style-6">
+        <h1>REGISTER</h1>
+        <form>
+          <input type="text" name="firstname" placeholder="First Name" />
+          <input type="text" name="lastname" placeholder="Last Name" />
+          <input type="email" name="email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Password" />
+          <input type="submit" value="Register" />
+        </form>
+      </div>
+  </body>
+</html>
